ec331e5be02fd8193a2beb1d3e7e183e6f600eab
Szedann <65607498+Szedann@users.noreply.github.com>
2024-03-10 23:45:46 +0100

    nooo I used a tutorial

--- src/client/java/dev/szedann/oddities/mixin/client/ItemRendererMixin.java
+++ src/client/java/dev/szedann/oddities/mixin/client/ItemRendererMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
     @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
-    public BakedModel useRubyStaffModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    public BakedModel useTemplateSignModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         if ((stack.isOf(SOItems.SIGN_TEMPLATE) && (stack.getNbt() != null && !stack.getNbt().getList("messages", NbtElement.STRING_TYPE).stream().map(NbtElement::asString).allMatch(String::isEmpty)))) {
             return ((ItemRendererAccessor) this).szoddities$getModels().getModelManager().getModel(new ModelIdentifier(Szoddities.MOD_ID, "sign_template_filled", "inventory"));
         }
